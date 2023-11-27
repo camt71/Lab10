1fb06a34462a3ec3fedafd6e13fda5ab80bf1945
camt71 <124553258+camt71@users.noreply.github.com>
2023-11-27 23:55:08 +0700

    Update MyLIFO_App.java

--- MyLIFO_App.java
+++ MyLIFO_App.java
@@ -54,9 +54,71 @@ public class MyLIFO_App {
 
 	// This method evaluates the value of an expression
 	// i.e. 51 + (54 *(3+2)) = 321
-	public static int evaluateExpression(String expression) {
-		return 0;
-	}
+	public static int evaluateExpression(String exp) {
+        Stack<Integer> operandStack = new Stack<>();
+        Stack<Character> operatorStack = new Stack<>();
+        String num = "";
+        for (int i = 0; i < exp.length(); i++) {
+            char currentChar = exp.charAt(i);
+            if (!isOpearator(currentChar)) {
+                num += currentChar;
+                if (i == exp.length() - 1) operandStack.push(Integer.parseInt(num));
+            } else {
+                if (!num.equals("")) {
+                    operandStack.push(Integer.parseInt(num));
+                }
+                switch (currentChar) {
+                    case ')': {
+                        while (operatorStack.peek() != '(') {
+                            operandStack.push(calc(operandStack.pop(), operatorStack.pop(), operandStack.pop()));
+                        }
+                        operatorStack.pop();
+                        break;
+                    }
+                    case '+', '-': {
+                        if (!operatorStack.isEmpty() && operandStack.size() > 1 && operatorStack.peek() != '(') {
+                            int test = calc(operandStack.pop(), operatorStack.pop(), operandStack.pop());
+                            operandStack.push(test);
+                        }
+                        operatorStack.push(currentChar);
+                        break;
+                    }
+                    case '*', '/': {
+                        if (!operatorStack.isEmpty())
+                            if (operatorStack.peek() == '*' || operatorStack.peek() == '/') {
+                                operandStack.push(calc(operandStack.pop(), operatorStack.pop(), operandStack.pop()));
+                            }
+                        operatorStack.push(currentChar);
+                        break;
+                    }
+                    default: operatorStack.push(currentChar);
+                }
+                num = "";
+            }
+        }
+        while (!operatorStack.isEmpty()) {
+            operandStack.push(calc(operandStack.pop(), operatorStack.pop(), operandStack.pop()));
+        }
+        return operandStack.pop();
+    }
+
+	public static boolean isOpearator(char input) {
+        return input == '+' || input == '-' || input == '*' || input == '/' || input == '(' || input == ')';
+    }
+
+    public static int calc(int a, char exp, int b) {
+        switch (exp) {
+            case '+':
+                return a + b;
+            case '-':
+                return b - a;
+            case '*':
+                return a * b;
+            case '/':
+                return a / b;
+        }
+        return 0;
+    }
 
 	public static void main(String[] args) {
 		Integer[] array = { 1, 2, 3, 4, 5, 6 };
